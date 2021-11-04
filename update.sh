a1f05641d2641c41675fcda3faa3d610004051fb
Purpux <icaliberdev@gmail.com>
2021-11-04 13:54:27 +0530

    added sway

--- update.sh
+++ update.sh
@@ -8,5 +8,5 @@ Array=(
 for i in "${Array[@]}"; do
 	cp $i $(echo $i|cut -d "." -f 2)
 done
-cp -r ~/.config/{tmux,python,neofetch,vim,irssi,procps} .
+cp -r ~/.config/{sway,tmux,python,neofetch,vim,irssi,procps} .
 # cp -r ~/.config/{X11,tmux,openbox,python,neofetch,vim,irssi,procps} .
